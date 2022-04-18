3885d9f2f646a47cb419e4bf7430d266358042cf
Raphae <raphae@pop-os.localdomain>
2022-04-18 02:29:01 -0300

    emit listener Nav for Parent Created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script>
-import { watch } from '@vue/runtime-core';
 import Error from './components/Error.vue'
 import Nav from './components/Nav.vue'
 
@@ -16,8 +15,10 @@ export default {
   data() {
     return {
       apiStatus: true,
-      chosenCripto: null, 
-      chosenVS: null 
+      chosen: {
+        cripto: null, 
+        VS: null
+      }
     }
   },
  
@@ -26,17 +27,45 @@ export default {
       const res = await fetch(`https://api.coingecko.com/api/v3/ping`)
       const data = await res.json()
       this.apiStatus = data.gecko_says === "(V3) To the Moon!"
+    },
+
+    async getCriptoValue() {
+      const { cripto, VS } = this.chosen
+      console.log(cripto != null, VS != null)
+      if( cripto != null && VS != null ) {
+        console.log(cripto, VS)
+      }
+    },
+
+    setChosenCripto(value) {
+      this.chosen.cripto = value
+    },
+
+    setChosenVS(value) {
+      this.chosen.VS = value
+    }
+  },
+
+  watch: {
+    chosen: {
+      handler() {
+        this.getCriptoValue()
+      },
+      deep: true
     }
   }
 
-};
+}
 </script>
 
 
 <template>
   <div v-if = 'apiStatus' class = 'container-fluid bg-dark text-white vh-100 px-0'> 
     <div class = 'row'></div>
-      <Nav/>
+      <Nav
+      @setChosenCripto = 'setChosenCripto'
+      @setChosenVS = 'setChosenVS'
+      />
     </div>  
   <Error v-else/>
 </template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,43 @@
  <script>
      export default {
-         name: "Nav"
+         name: "Nav",
+         data() {
+             return {
+                 chosenCripto: "CHOOSE YOUR CRIPTO HERE",
+                 chosenVS: "CHOOSE YOUR VS CURRENT HERE" 
+             }
+         },
+
+         watch: {
+             chosenCripto() {
+                 this.$emit('setChosenCripto', this.chosenCripto)
+             },
+             chosenVS() {
+                 this.$emit('setChosenVS', this.chosenVS)
+             }
+             
+         }
+         
      }
  </script>
 
  <template>
     <nav class = 'px-1 navbar-expand bg-secondary'>
         <img class = 'h-100' src = "https://static.coingecko.com/s/coingecko-mascot-suit-b1a9df2b041094a017948f1d184f1aa263e779d4e1f22c437e835b74f0b00073.png" ><span class = 'fs-3'>CriptoCurrency</span>
-        <select class="text-white form-select bg-secondary w-25 d-inline mx-3" aria-label="Default select example">
+        <select v-model = 'chosenCripto' class="text-white form-select bg-secondary w-25 d-inline mx-3" aria-label="Default select example">
             <option hidden disabled selected>CHOOSE YOUR CRIPTO HERE</option>
-            <option name = 'bitcoin' value="1">Bitcoin</option>
-            <option name = 'ethereum' value="2">ETH</option>
-            <option name = 'dacxi' value="3">DACXI</option>
-            <option name = 'cosmos' value="3">ATOM</option>
-            <option name = 'terra-luna' value="3">LUNA</option>
+            <option value="bitcoin">Bitcoin</option>
+            <option value="ethereum">ETH</option>
+            <option value="dacxi">DACXI</option>
+            <option value="cosmos">ATOM</option>
+            <option value="terra-luna">LUNA</option>
         </select>
         <span class = 'fs-5'> VS </span>
-        <select class="text-white form-select bg-secondary w-25 d-inline mx-3" aria-label="Default select example">
+        <select v-model = 'chosenVS' class="text-white form-select bg-secondary w-25 d-inline mx-3" aria-label="Default select example">
             <option hidden disabled selected>CHOOSE YOUR VS CURRENT HERE</option>
-            <option class = 'text-uppercase' value="1">brl</option>
-            <option class = 'text-uppercase' value="2">usb</option>
-            <option class = 'text-uppercase' value="3">eur</option>
+            <option class = 'text-uppercase' value="brl">BRL</option>
+            <option class = 'text-uppercase' value="usd">USD</option>
+            <option class = 'text-uppercase' value="eur">EUR</option>
         </select>
         <input type = 'date' class = 'w-25 text-white d-inline bg-secondary form-control'>
 
@@ -29,10 +46,10 @@
  </template>
 
  <style>
-     img { 
+    img { 
          height: 30px;
          width: 30px;
          position: relative;
          bottom: 5px;
-     }
+        }
  </style>
